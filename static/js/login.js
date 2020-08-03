b681412dd9484637c8e76bdb22ea3f3d0285a3b6
z122677 <1226776310@qq.com>
2020-08-03 14:57:27 +0800

    抽取了公共页面

--- static/js/login.js
+++ static/js/login.js
@@ -1,9 +1,69 @@
+// 登录 - 验证逻辑
+
+
 function login() {
-    $.post({
-        url:"/login",
-        data:$('#login').serialize(),
-        success:function (data) {
-            alert("你获取的数据为"+data)
+    let stu_code = $("#username").val();
+    let password = $("#password").val();
+    let check_code = $("#check_code").val();
+    $.ajax({
+        url: back_host + "login/",
+        data: {
+            "stu_code": stu_code,
+            "password": password,
+            "check_code": check_code
+        },
+        type: "POST",
+        dataType: "json",
+        async: false,
+        success: data => {
+            console.log(data);
+            if( data.code === 400)
+            {
+                alert(data.msg);
+                return;
+            }
+            localStorage.setItem("token", data.token);
+            top.location = 'index.html';
+        },
+    });
+
+}
+
+function getCode() {
+    $.ajax({
+        type: "GET",
+        url: back_host + "login/",
+        success: data => {
+            $(".field img").attr("src", data)
         }
     })
 }
+
+function auth() {
+    let token = localStorage.getItem("token");
+    if (token === "") {
+        top.location = 'login.html';
+    }
+    else {
+        $.ajax({
+            type: "POST",
+            data: {
+                "token": token
+            },
+            dataType: "json",
+            url: back_host + "auth/",
+            async: false,
+            success: data => {
+                if (data.code === 400) {
+                    alert(data.msg);
+                    top.location = "login.html"
+                }
+                $(".menu img").attr("src", data.user.avatar_url);
+                $(".item span").text(data.user.username);
+            }
+        })
+    }
+}
+
+
+
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+var back_host = "http://localhost:8000/";
+var front_host = "http://localhost:63343/";
+
+
+
+
+
+
+
